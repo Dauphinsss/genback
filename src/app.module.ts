77e4bd3144ebdec76610a70483b43babba3d2b30
Daniel_Virreira <virreira.daniel@gmail.com>
2025-10-01 02:01:00 -0400

    feat(settings): integra módulo de upload con la aplicación

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [AuthModule, PrismaModule, UploadModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
