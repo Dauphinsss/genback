ec870b265952b0bf1e64f3165c7de3fa30df4717
Vicente Guzman Aliza Abigail <alizaabigailvicenteguzman@gmail.com>
2025-10-15 04:31:41 -0400

    feat(prisma): reinitialize schema with updated models

---
+++ prisma/migrations/20251015020837_init/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `index` to the `Lesson` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Lesson" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "index" INTEGER NOT NULL,
+    "duration" INTEGER,
+    "content" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "unitId" INTEGER NOT NULL,
+    CONSTRAINT "Lesson_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Unit" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Lesson" ("content", "createdAt", "duration", "id", "title", "unitId", "updatedAt") SELECT "content", "createdAt", "duration", "id", "title", "unitId", "updatedAt" FROM "Lesson";
+DROP TABLE "Lesson";
+ALTER TABLE "new_Lesson" RENAME TO "Lesson";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
